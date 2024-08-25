eacf28c85760640c361bec1bd7e3e1186b97d01d
lalit1711 <lalit.sharma.ls45@gmail.com>
2024-08-25 22:24:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World from ECS!");
